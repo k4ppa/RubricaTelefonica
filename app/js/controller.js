4d58b198c688015b5057799f6822e0d8ac57c73a
Mattia Cappa <cappa.mattia@gmail.com>
2014-12-22 17:20:50 +0100

    Rimosso stub rappresentante i contatti
    I contatti sono stati spostati dal controller a un file json i cui dati
    vengono invocati mediante una chiamata GET

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,15 +1,7 @@
 var rubricaTelefonicaApp = angular.module('rubricaTelefonicaApp', []);
 
-rubricaTelefonicaApp.controller('ContactListCtrl', function ($scope) {
-    $scope.contacts = [
-        {'firstName': 'Mattia',
-            'lastName': 'Cappa',
-            'phoneNumber': '+39 335 1411077'},
-        {'firstName': 'Mattia',
-            'lastName': 'Conversano',
-            'phoneNumber': '+39 335 1234567'},
-        {'firstName': 'Daniele',
-            'lastName': 'Buratti',
-            'phoneNumber': '+39 335 7654321'}
-    ];
+rubricaTelefonicaApp.controller('ContactListCtrl', function ($scope, $http) {
+    $http.get('contacts/contacts.json').success(function(data) {
+        $scope.contacts = data;
+    });
 });
